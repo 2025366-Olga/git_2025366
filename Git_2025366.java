d6fd36f18e755c7df3c6ac45fec1f825a07f54ce
2025366-Olga <2025366@student.cct.ie>
2025-12-07 19:33:25 +0000

    added a simple print

---
+++ Git_2025366.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2025366;
+
+/**
+ *
+ * @author olgar
+ */
+public class Git_2025366 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Olga - 2025366");  
+     
+      
+
+    }
+    
+}
